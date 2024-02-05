9ebdaba95c9fc5fb7f260ea63d4fdb9cea948545
Victor Carrara Polido Loureiro <vcarrara@c2r1p4.42sp.org.br>
2024-02-05 18:01:55 -0300

    final?

--- src/move.c
+++ src/move.c
@@ -6,7 +6,7 @@
 /*   By: vcarrara <vcarrara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 18:20:05 by vcarrara          #+#    #+#             */
-/*   Updated: 2024/02/04 13:07:10 by vcarrara         ###   ########.fr       */
+/*   Updated: 2024/02/05 17:51:49 by vcarrara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,34 +38,6 @@ static void	next_step(t_data *data, int32_t x, int32_t y)
 	data->map->matrix[y][x] = 'P';
 }
 
-void	up_move(t_data *data, int32_t move_x, int32_t move_y)
-{
-	int32_t	x;
-	int32_t	y;
-
-	x = move_x;
-	y = move_y - 1;
-	if (data->map->matrix[y][x] != '1')
-	{
-		next_step(data, x, y);
-		move_y--;
-	}
-}
-
-void	down_move(t_data *data, int32_t move_x, int32_t move_y)
-{
-	int32_t	x;
-	int32_t	y;
-
-	x = move_x;
-	y = move_y + 1;
-	if (data->map->matrix[y][x] != '1')
-	{
-		next_step(data, x, y);
-		move_y++;
-	}
-}
-
 void	left_move(t_data *data, int32_t move_x, int32_t move_y)
 {
 	int32_t	x;
@@ -73,11 +45,26 @@ void	left_move(t_data *data, int32_t move_x, int32_t move_y)
 
 	x = move_x - 1;
 	y = move_y;
-	data->content->side_player = 1;
+	data->content->side_player = 0;
 	if (data->map->matrix[y][x] != '1')
 	{
 		next_step(data, x, y);
 		move_x--;
+	}
+}
+
+void	up_move(t_data *data, int32_t move_x, int32_t move_y)
+{
+	int32_t	x;
+	int32_t	y;
+
+	x = move_x;
+	y = move_y - 1;
+	data->content->side_player = 1;
+	if (data->map->matrix[y][x] != '1')
+	{
+		next_step(data, x, y);
+		move_y--;
 	}
 }
 
@@ -88,10 +75,25 @@ void	right_move(t_data *data, int32_t move_x, int32_t move_y)
 
 	x = move_x + 1;
 	y = move_y;
-	data->content->side_player = 0;
+	data->content->side_player = 2;
 	if (data->map->matrix[y][x] != '1')
 	{
 		next_step(data, x, y);
 		move_x++;
+	}
+}
+
+void	down_move(t_data *data, int32_t move_x, int32_t move_y)
+{
+	int32_t	x;
+	int32_t	y;
+
+	x = move_x;
+	y = move_y + 1;
+	data->content->side_player = 3;
+	if (data->map->matrix[y][x] != '1')
+	{
+		next_step(data, x, y);
+		move_y++;
 	}
 }
--- src/render_move.c
+++ src/render_move.c
@@ -6,7 +6,7 @@
 /*   By: vcarrara <vcarrara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 18:42:19 by vcarrara          #+#    #+#             */
-/*   Updated: 2024/01/29 18:44:40 by vcarrara         ###   ########.fr       */
+/*   Updated: 2024/02/05 17:17:44 by vcarrara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,33 +14,7 @@
 
 void	render_move_player_v(t_data *data, int32_t col, int32_t row)
 {
-	if (data->content->side_player == 0)
-	{
-		mlx_delete_image(data->mlx, data->content->player_img);
-		mlx_delete_texture(data->content->player_png);
-		data->content->player_png = mlx_load_png(PLAYER_R);
-		data->content->player_img = mlx_texture_to_image(data->mlx, \
-			data->content->player_png);
-		mlx_resize_image(data->content->player_img, SIZE_IMG, SIZE_IMG);
-		mlx_image_to_window(data->mlx, data->content->player_img, \
-			(SIZE_IMG * col), (SIZE_IMG * row));
-	}
-	else if (data->content->side_player == 1)
-	{
-		mlx_delete_image(data->mlx, data->content->player_img);
-		mlx_delete_texture(data->content->player_png);
-		data->content->player_png = mlx_load_png(PLAYER_L);
-		data->content->player_img = mlx_texture_to_image(data->mlx, \
-			data->content->player_png);
-		mlx_resize_image(data->content->player_img, SIZE_IMG, SIZE_IMG);
-		mlx_image_to_window(data->mlx, data->content->player_img, \
-			(SIZE_IMG * col), (SIZE_IMG * row));
-	}
-}
-
-void	render_move_player_h(t_data *data, int32_t col, int32_t row)
-{
-	if (data->content->side_player == 2)
+	if (data->content->side_player == 1)
 	{
 		mlx_delete_image(data->mlx, data->content->player_img);
 		mlx_delete_texture(data->content->player_png);
@@ -56,6 +30,32 @@ void	render_move_player_h(t_data *data, int32_t col, int32_t row)
 		mlx_delete_image(data->mlx, data->content->player_img);
 		mlx_delete_texture(data->content->player_png);
 		data->content->player_png = mlx_load_png(PLAYER_D);
+		data->content->player_img = mlx_texture_to_image(data->mlx, \
+			data->content->player_png);
+		mlx_resize_image(data->content->player_img, SIZE_IMG, SIZE_IMG);
+		mlx_image_to_window(data->mlx, data->content->player_img, \
+			(SIZE_IMG * col), (SIZE_IMG * row));
+	}
+}
+
+void	render_move_player_h(t_data *data, int32_t col, int32_t row)
+{
+	if (data->content->side_player == 0)
+	{
+		mlx_delete_image(data->mlx, data->content->player_img);
+		mlx_delete_texture(data->content->player_png);
+		data->content->player_png = mlx_load_png(PLAYER_L);
+		data->content->player_img = mlx_texture_to_image(data->mlx, \
+			data->content->player_png);
+		mlx_resize_image(data->content->player_img, SIZE_IMG, SIZE_IMG);
+		mlx_image_to_window(data->mlx, data->content->player_img, \
+			(SIZE_IMG * col), (SIZE_IMG * row));
+	}
+	else if (data->content->side_player == 2)
+	{
+		mlx_delete_image(data->mlx, data->content->player_img);
+		mlx_delete_texture(data->content->player_png);
+		data->content->player_png = mlx_load_png(PLAYER_R);
 		data->content->player_img = mlx_texture_to_image(data->mlx, \
 			data->content->player_png);
 		mlx_resize_image(data->content->player_img, SIZE_IMG, SIZE_IMG);
